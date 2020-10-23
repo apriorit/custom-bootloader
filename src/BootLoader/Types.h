a2c3064865d3a8da7fd2f6f186224f8c1486f34b
Vladimir Vasilevskiy <vladimir.vasilevskiy@dev.local>
2020-10-23 15:24:59 +0300

    added vs2019 sln and dmc compiler

--- src/BootLoader/Types.h
+++ src/BootLoader/Types.h
@@ -6,9 +6,5 @@
 typedef unsigned char   byte;
 typedef unsigned short  word;
 typedef unsigned long   dword;
-typedef char            bool;
-
-#define true            0x1
-#define false           0x0
 
 #endif // __TYPES__
